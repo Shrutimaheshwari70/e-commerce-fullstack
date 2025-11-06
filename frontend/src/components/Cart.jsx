86b716d902e146f7fce7a26b6005113165bef7b9
ishita jain <ishitajain385@gmail.com>
2025-11-06 23:09:31 +0530

    ..

--- frontend/src/components/Cart.jsx
+++ frontend/src/components/Cart.jsx
@@ -33,7 +33,7 @@ export default function Cart() {
     const fetchCart = async () => {
       setLoading(true);
       try {
-        const res = await fetch("https://e-com-project-1-db0p.onrender.com/cart/get", {
+        const res = await fetch("https://e-com-project-msn4.onrender.com/cart/get", {
           method: "GET",
           credentials: "include", //cookie 
         });
@@ -51,7 +51,7 @@ export default function Cart() {
 
   const fetchAddresses = async () => {
     try {
-      const res = await fetch("https://e-com-project-1-db0p.onrender.com/address/get", {
+      const res = await fetch("https://e-com-project-msn4.onrender.com/address/get", {
         method: "GET",
         credentials: "include",
       });
@@ -75,7 +75,7 @@ export default function Cart() {
     setUpdating(true);
 
     try {
-      const res = await fetch("https://e-com-project-1-db0p.onrender.com/cart/update", {
+      const res = await fetch("https://e-com-project-msn4.onrender.com/cart/update", {
         method: "POST",
         credentials: "include",
         headers: { "Content-Type": "application/json" },
@@ -105,7 +105,7 @@ export default function Cart() {
     setSendingOtp(true);
 
     try {
-      const res = await fetch("https://e-com-project-1-db0p.onrender.com/otp/send", {
+      const res = await fetch("https://e-com-project-msn4.onrender.com/otp/send", {
         method: "POST",
         credentials: "include",
         headers: { "Content-Type": "application/json" },
@@ -139,7 +139,7 @@ export default function Cart() {
     setVerifyingOtp(true);
 
     try {
-      const res = await fetch("https://e-com-project-1-db0p.onrender.com/otp/verify", {
+      const res = await fetch("https://e-com-project-msn4.onrender.com/otp/verify", {
         method: "POST",
         credentials: "include",
         headers: { "Content-Type": "application/json" },
@@ -183,7 +183,7 @@ export default function Cart() {
     }
 
     try {
-      const res = await fetch("https://e-com-project-1-db0p.onrender.com/address/add", {
+      const res = await fetch("https://e-com-project-msn4.onrender.com/address/add", {
         method: "POST",
         credentials: "include",
         headers: { "Content-Type": "application/json" },
