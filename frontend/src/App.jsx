82ce72789f8af11300887579ae45d8939d78c2c7
ishita jain <ishitajain385@gmail.com>
2025-11-06 12:50:46 +0530

    chnged export statement

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,37 +1,32 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import './App.css'
-import Navbar from './components/Navbar.jsx'
+import "./App.css";
+import Navbar from "./components/Navbar.jsx";
 
-import Profile from './components/Profile.jsx'
-import Shop from './components/Shop.jsx'
+import Profile from "./components/Profile.jsx";
+import Shop from "./components/Shop.jsx";
 import AdminPanel from "./components/AdminPanel";
-import {ContextProvider} from "./Context/contextProvider.jsx";
+import  ContextProvider  from "./Context/contextProvider.jsx";
 import ShopCtaegory from "./components/ShopCtaegory.jsx";
 import Cart from "./components/Cart.jsx";
 import Orders from "./components/Orders.jsx";
 
-
-
-
-export default function App(){
-    return (
-        <>
-        <Navbar/>
-             <ContextProvider>
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <ContextProvider>
         <Routes>
-            <Route path="/" element={<Shop/>}/>
-         
-            <Route path="/Shop" element={<Shop/>}/>
-            <Route path="/profile" element={<Profile/>}/>
-            <Route path="/admin" element={<AdminPanel/>}/>
-       <Route path="/cart" element={<Cart/>}/>
-  <Route path="/orders" element={<Orders/>}/>
-      <Route path="/shop/:category" element={<ShopCtaegory/>}/>
-           
-        </Routes>
-         </ContextProvider>
-        </>
-    )
+          <Route path="/" element={<Shop />} />
 
+          <Route path="/Shop" element={<Shop />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/admin" element={<AdminPanel />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/shop/:category" element={<ShopCtaegory />} />
+        </Routes>
+      </ContextProvider>
+    </>
+  );
 }
