d830f937b1d03bde7b6199d420706c19afa7313d
Shruti Maheshwari <your_email@example.com>
2025-11-06 10:56:52 +0530

    Fix contextProvider import case for Render

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import Navbar from './components/Navbar.jsx'
 import Profile from './components/Profile.jsx'
 import Shop from './components/Shop.jsx'
 import AdminPanel from "./components/AdminPanel";
-import ContextProvider from "./Context/contextProvider.jsx";
+import ContextProvider from "./Context/ContextProvider.jsx"
 import ShopCtaegory from "./components/ShopCtaegory.jsx";
 import Cart from "./components/Cart.jsx";
 import Orders from "./components/Orders.jsx";
